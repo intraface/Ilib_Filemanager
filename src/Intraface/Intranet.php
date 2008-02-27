c7064a9c21b16d6fdb95f90cfcf69f9d506c9fa0
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-02-27 14:58:51 +0000

    Changed the fake get

--- src/Intraface/Intranet.php
+++ src/Intraface/Intranet.php
@@ -2,13 +2,16 @@
 
 class FakeIntranet {
 
-
+    public $values;
+    
+    public function __construct() {
+        $this->values = array(
+            'id' => 0,
+            'public_key' => 'file');
+    }
+    
     public function get($key) {
         // if id should be other than 0 you need to add options to the use of Ilib classes with 'intranet_id = ?'
-        $values = array(
-            'id' => 0,
-            'public_key' => 'vih');
-
-        return $values[$key];
+        return $this->values[$key];
     }
 }
