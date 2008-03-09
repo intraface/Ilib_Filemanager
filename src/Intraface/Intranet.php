8d47930a54e24b32a3777461daa6ce0604949dde
lsolesen <lsolesen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-03-09 19:09:38 +0000

    Coding standard improvements.

--- src/Intraface/Intranet.php
+++ src/Intraface/Intranet.php
@@ -1,16 +1,17 @@
 <?php
-
-class FakeIntranet {
-
+class FakeIntranet
+{
     public $values;
-    
-    public function __construct() {
+
+    public function __construct()
+    {
         $this->values = array(
             'id' => 0,
             'public_key' => 'file');
     }
-    
-    public function get($key) {
+
+    public function get($key)
+    {
         // if id should be other than 0 you need to add options to the use of Ilib classes with 'intranet_id = ?'
         return $this->values[$key];
     }
