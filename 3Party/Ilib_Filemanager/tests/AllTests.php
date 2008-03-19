6c395021cd6c3a9796c3d2735fe567be41f55ce0
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-03-19 17:17:22 +0000

    Updated tests

--- 3Party/Ilib_Filemanager/tests/AllTests.php
+++ 3Party/Ilib_Filemanager/tests/AllTests.php
@@ -2,13 +2,13 @@
 require_once 'PHPUnit/Framework.php';
 require_once 'PHPUnit/TextUI/TestRunner.php';
 
-class FileHandler_AllTests
+class AllTests
 {
     public static function suite()
     {
         $suite = new PHPUnit_Framework_TestSuite('Intraface_FileHandler');
 
-        $tests = array('FileViewer', 'FileHandler', 'AppendFile', 'InstanceHandler', 'InstanceManager', 'TemporaryFile', 'ImageHandler', 'ImageRandomizer');
+        $tests = array('FileViewer', 'FileHandler', 'AppendFile', 'InstanceHandler', 'InstanceManager', 'TemporaryFile', 'ImageHandler', 'ImageRandomizer', 'FileManager');
 
         foreach ($tests AS $test) {
             require_once $test . 'Test.php';
