ffd8f7d5c28ab71905c79b53471873d77dbfe24e
sune.t.jensen <sune.t.jensen@2fc64ab5-e821-0410-98e1-ee8f871d7b73>
2008-09-24 17:46:04 +0000

    Files now have permission 644 when uploaded. To make sure it is possible to backup the files. This is a security issue, as all users on server has access to read the file.

--- 3Party/Ilib_Filemanager/src/Ilib/Filehandler/UploadHandler.php
+++ 3Party/Ilib_Filemanager/src/Ilib/Filehandler/UploadHandler.php
@@ -378,6 +378,11 @@ class Ilib_Filehandler_UploadHandler extends Ilib_Filehandler_Standard
                     $file_handler->delete();
                     continue;
                 }
+                
+                if(!chmod($this->upload_path.$server_file_name, 0644)) {
+                    // please do not stop executing here
+                    trigger_error("Unable to chmod file '".$this->upload_path.$server_file_name."'", E_USER_NOTICE);
+                }
             }
         }
 
